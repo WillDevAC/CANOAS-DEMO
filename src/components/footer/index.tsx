0692727674f0ed3247efeb3df2a969dffd86304e
WillDevAc <wydemairomsilva@gmail.com>
2022-04-02 11:39:02 -0500

    feature/add-password-dashboard and fix

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -13,9 +13,9 @@ const footer: React.FC = () => {
         <img src="images/logo.webp" alt="" />
       </Logo>
       <Icons>
-        <a href="#"><FaFacebookF/></a>
-        <a href="#"><FaInstagram/></a>
-        <a href="#"><FaWhatsapp/></a>
+        <a href="https://web.facebook.com/escolainfantilalgodaodoceemcanoas" target="__BLANK"><FaFacebookF/></a>
+        <a href="https://www.instagram.com/escolaalgodaodocecanoas" target="__BLANK"><FaInstagram/></a>
+        <a href="https://api.whatsapp.com/send?phone=5551994783328&text=Olá gostaria de falar com vocês." target="__BLANK"><FaWhatsapp/></a>
       </Icons>
     </Container>
   );
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import LayoutDashboard from '../../components/dashboard/layout'
 
 import Card from '../../components/dashboard/card'
 
+import { useRouter } from 'next/router';
+
 const dashboard: React.FC = () => {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    
+    const password = prompt('Digite sua senha: ');
+
+    if(password != '0225'){
+      router.push('/')
+    }
+
+  });
+
   return (
     <LayoutDashboard>
       <Card Title="Cardápio Berçário" Date="03/03/2022" Link="menus/cardapio.xlsx"/>
